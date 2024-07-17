c5a14166efb2cbef43aec7439092a404b3c3c326
seungdangdang <yseungun.21@gmail.com>
2024-07-18 01:41:23 +0900

    feat: 체험단 검색을 위한 mybatis 매핑 쿼리문 작성

--- src/main/java/com/nuguna/freview/admin/mapper/AdminMapper.java
+++ src/main/java/com/nuguna/freview/admin/mapper/AdminMapper.java
@@ -12,6 +12,7 @@ public interface AdminMapper {
 
   AdminVO selectAdmin(Long userSeq);
   List<CustomerListDTO> selectCustomerList(CustomerListRequestDTO dto);
+  List<CustomerListDTO> searchCustomer(@Param("previousUserSeq") Long previousUserSeq, @Param("searchWord") String searchWord, @Param("limit") Integer limit);
   int selectMatchingAdmin(@Param("adminSeq") Long userSeq, @Param("password")String password);
   int deleteUser(Long userSeq);
 }
