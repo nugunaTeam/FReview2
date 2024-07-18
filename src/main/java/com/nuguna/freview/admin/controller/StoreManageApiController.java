ee6878e85e5a0c8712ac89adb8fc227781fec2cd
seungdangdang <yseungun.21@gmail.com>
2024-07-18 14:56:32 +0900

    feat: 스토어 탈퇴 API 메서드 구현

--- src/main/java/com/nuguna/freview/admin/controller/StoreManageApiController.java
+++ src/main/java/com/nuguna/freview/admin/controller/StoreManageApiController.java
@@ -4,12 +4,17 @@ import com.nuguna.freview.admin.dto.request.StoreListRequestDTO;
 import com.nuguna.freview.admin.dto.response.StoreListDTO;
 import com.nuguna.freview.admin.dto.response.page.StoreManageResponseDTO;
 import com.nuguna.freview.admin.service.AdminService;
+import com.nuguna.freview.global.util.ShaUtil;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -40,5 +45,21 @@ public class StoreManageApiController {
         .build();
 
     return responseDTO;
+  }
+
+  @RequestMapping(value = "/{deleteUserSeq}", method = RequestMethod.DELETE)
+  public ResponseEntity<?> deleteStore(@PathVariable Long deleteUserSeq,
+      @RequestParam String adminVerificationPW,
+      @RequestParam Long adminSeq) {
+    String encodedPW = ShaUtil.sha256Encoding(adminVerificationPW);
+
+    try {
+      adminService.deleteUser(adminSeq, encodedPW, deleteUserSeq);
+      return new ResponseEntity<>(HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    } catch (IllegalStateException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
