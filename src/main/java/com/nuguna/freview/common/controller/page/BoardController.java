1905892ff0720aa584e7af0511898ffc59b27b76
seungdangdang <yseungun.21@gmail.com>
2024-07-19 08:32:32 +0900

    feat: 특정 공지글을 조회하는 page 컨트롤러 구현

--- src/main/java/com/nuguna/freview/common/controller/page/BoardController.java
+++ src/main/java/com/nuguna/freview/common/controller/page/BoardController.java
@@ -1,11 +1,14 @@
 package com.nuguna.freview.common.controller.page;
 
+import com.nuguna.freview.common.dto.response.page.NoticeDetailResponseDTO;
+import com.nuguna.freview.common.service.PostService;
 import com.nuguna.freview.common.service.UserService;
 import com.nuguna.freview.common.vo.user.UserVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,10 +18,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class BoardController {
 
   private final UserService userService;
+  private final PostService postService;
 
   @Autowired
-  public BoardController(UserService userService) {
+  public BoardController(UserService userService, PostService postService) {
     this.userService = userService;
+    this.postService = postService;
   }
 
   @RequestMapping(value = "/notice", method = RequestMethod.GET)
@@ -30,5 +35,18 @@ public class BoardController {
     model.addAttribute("loginUser", loginUser);
 
     return "common-notice-board";
+  }
+
+  @RequestMapping(value = "/notice/{postSeq}", method = RequestMethod.GET)
+  public String noticePostDetail(@PathVariable Long postSeq, Model model) {
+    //HACK: 로그인 유저의 실제 seq 로 수정 필요
+    Long userSeq = 301L;
+    UserVO loginUser = userService.getUserInfo(userSeq);
+    model.addAttribute("loginUser", loginUser);
+
+    NoticeDetailResponseDTO currentPost = postService.getPostBySeq(postSeq);
+    model.addAttribute("currentPost", currentPost);
+
+    return "common-notice-detail";
   }
 }
