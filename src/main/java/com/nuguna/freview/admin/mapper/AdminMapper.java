7ab28a7af3756d86530848baee69c06a034000bd
seungdangdang <yseungun.21@gmail.com>
2024-07-18 14:33:04 +0900

    feat: 스토어 조회 및 검색 결과를 조회하는 mapper 메서드 작성

--- src/main/java/com/nuguna/freview/admin/mapper/AdminMapper.java
+++ src/main/java/com/nuguna/freview/admin/mapper/AdminMapper.java
@@ -1,6 +1,7 @@
 package com.nuguna.freview.admin.mapper;
 
 import com.nuguna.freview.admin.dto.response.CustomerListDTO;
+import com.nuguna.freview.admin.dto.response.StoreListDTO;
 import com.nuguna.freview.admin.vo.AdminVO;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
@@ -12,6 +13,8 @@ public interface AdminMapper {
   AdminVO selectAdmin(Long userSeq);
   List<CustomerListDTO> selectCustomerList(@Param("previousUserSeq") Long previousUserSeq, @Param("limit") Integer limit);
   List<CustomerListDTO> searchCustomer(@Param("previousUserSeq") Long previousUserSeq, @Param("searchWord") String searchWord, @Param("limit") Integer limit);
+  List<StoreListDTO> selectStoreList(@Param("previousUserSeq") Long previousUserSeq, @Param("limit") Integer limit);
+  List<StoreListDTO> searchStore(@Param("previousUserSeq") Long previousUserSeq, @Param("searchWord") String searchWord, @Param("limit") Integer limit);
   int selectMatchingAdmin(@Param("adminSeq") Long userSeq, @Param("password")String password);
   int deleteUser(Long userSeq);
 }
