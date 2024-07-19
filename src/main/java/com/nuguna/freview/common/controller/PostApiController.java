555bdf181ee5143dec516b3ce58044e84b95afd6
seungdangdang <yseungun.21@gmail.com>
2024-07-20 00:24:13 +0900

    feat: 게시글에 좋아요를 추가하는 API 컨트롤러 구현

---
+++ src/main/java/com/nuguna/freview/common/controller/PostApiController.java
@@ -0,0 +1,43 @@
+package com.nuguna.freview.common.controller;
+
+import com.nuguna.freview.common.dto.request.AddLIkeRequestDTO;
+import com.nuguna.freview.common.service.PostService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/common/post")
+public class PostApiController {
+
+  private final PostService postService;
+
+  @Autowired
+  public PostApiController(PostService postService) {
+    this.postService = postService;
+  }
+
+  @RequestMapping(value = "/like-add", method = RequestMethod.POST)
+  public ResponseEntity<?> addLikeToPost(@RequestBody AddLIkeRequestDTO requestDTO) {
+    Long postSeq = requestDTO.getPostSeq();
+    Long userSeq = requestDTO.getUserSeq();
+
+    try {
+      boolean result = postService.addLikeToPost(postSeq, userSeq);
+      if (result) {
+        return new ResponseEntity<>(HttpStatus.OK);
+      } else {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    } catch (Exception e) {
+      log.error("[ERROR] 게시글 좋아요 도중 에러 발생", e);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
