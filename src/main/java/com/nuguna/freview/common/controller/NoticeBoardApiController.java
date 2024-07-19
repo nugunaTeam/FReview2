417835af3a00a6878d7102295c5018f8ebea66cc
seungdangdang <yseungun.21@gmail.com>
2024-07-19 10:09:53 +0900

    feat: 특정 공지글을 수정하는 API 컨트롤러 구현

--- src/main/java/com/nuguna/freview/common/controller/NoticeBoardApiController.java
+++ src/main/java/com/nuguna/freview/common/controller/NoticeBoardApiController.java
@@ -1,13 +1,20 @@
 package com.nuguna.freview.common.controller;
 
+import com.nuguna.freview.common.dto.request.NoticeUpdateRequestDTO;
 import com.nuguna.freview.common.dto.response.NoticeListDTO;
 import com.nuguna.freview.common.dto.response.page.NoticeResponseDTO;
 import com.nuguna.freview.common.service.BoardService;
+import com.nuguna.freview.common.service.PostService;
 import com.nuguna.freview.common.vo.post.PostCode;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,11 +26,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoticeBoardApiController {
 
   private final BoardService boardService;
+  private final PostService postService;
   private final int PAGE_SIZE = 10;
 
   @Autowired
-  public NoticeBoardApiController(BoardService boardService) {
+  public NoticeBoardApiController(BoardService boardService, PostService postService) {
     this.boardService = boardService;
+    this.postService = postService;
   }
 
   @RequestMapping(value = "/list/{currentPage}", method = RequestMethod.POST)
@@ -39,4 +48,25 @@ public class NoticeBoardApiController {
 
     return responseDTO;
   }
+
+  @RequestMapping(value = "/update", method = RequestMethod.PUT)
+  public ResponseEntity<?> updatePost(@RequestBody NoticeUpdateRequestDTO requestDTO) {
+    Long postSeq = requestDTO.getSeq();
+    String title = requestDTO.getTitle();
+    String content = requestDTO.getContent();
+    Timestamp now = Timestamp.valueOf(LocalDateTime.now());
+
+    try {
+      boolean updateResult = postService.updateNotice(postSeq, title, content, now);
+      if (updateResult) {
+        return new ResponseEntity<>(HttpStatus.OK);
+      } else {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    } catch (Exception e) {
+      log.error("[ERROR] 공지글 수정 도중 에러 발생", e);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
 }
