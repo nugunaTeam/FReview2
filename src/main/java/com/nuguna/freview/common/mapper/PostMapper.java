744e49ccee7fba30504b008c6d256413e6e0f527
seungdangdang <yseungun.21@gmail.com>
2024-07-19 10:11:07 +0900

    feat: 특정 공지글을 업데이트하는 mybatis 매퍼 파일 구현

--- src/main/java/com/nuguna/freview/common/mapper/PostMapper.java
+++ src/main/java/com/nuguna/freview/common/mapper/PostMapper.java
@@ -11,4 +11,8 @@ public interface PostMapper {
   NoticeDetailResponseDTO selectNotice(Long postSeq);
   int checkPostLiked(@Param("userSeq") Long userSeq, @Param("postSeq") Long postSeq);
   void insertPostViewCount(@Param("postSeq") Long postSeq);
+  int updateNotice(@Param("postSeq") Long postSeq,
+      @Param("title") String title,
+      @Param("content") String content,
+      @Param("updatedAt") Timestamp updatedAt);
 }
