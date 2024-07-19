5d3fa0b511f51d9ea62ab3ad76a8a0e0dbbf6cbc
seungdangdang <yseungun.21@gmail.com>
2024-07-19 15:35:35 +0900

    feat: 모집글 리스트를 가져오는 mybatis 매퍼 파일 구현

---
+++ src/main/java/com/nuguna/freview/common/mapper/MojipMapper.java
@@ -0,0 +1,12 @@
+package com.nuguna.freview.common.mapper;
+
+import com.nuguna.freview.common.dto.response.MojipPostDTO;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MojipMapper {
+
+  List<MojipPostDTO> selectMojipList(@Param("previousPostSeq") Long previousPostSeq, @Param("limit") int limit);
+}
