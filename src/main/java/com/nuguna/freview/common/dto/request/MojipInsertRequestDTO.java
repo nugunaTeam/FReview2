d606069ecd3ee66103c3f9cfae19e0d484101fe9
seungdangdang <yseungun.21@gmail.com>
2024-07-19 22:24:06 +0900

    feat: 모집글을 등록하는 mybatis 매퍼 파일 구현

---
+++ src/main/java/com/nuguna/freview/common/dto/request/MojipInsertRequestDTO.java
@@ -0,0 +1,21 @@
+package com.nuguna.freview.common.dto.request;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MojipInsertRequestDTO {
+
+  private Long userSeq;
+  private String title;
+  private Timestamp applyStartDate;
+  private Timestamp applyEndDate;
+  private Timestamp experienceDate;
+  private String content;
+}
--- src/main/java/com/nuguna/freview/common/mapper/MojipMapper.java
+++ src/main/java/com/nuguna/freview/common/mapper/MojipMapper.java
@@ -1,6 +1,7 @@
 package com.nuguna.freview.common.mapper;
 
-import com.nuguna.freview.common.dto.response.MojipPostDTO;
+import com.nuguna.freview.common.dto.response.MojipPostDetailDTO;
+import java.util.Date;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -8,7 +9,10 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface MojipMapper {
 
-  List<MojipPostDTO> selectMojipList(@Param("previousPostSeq") Long previousPostSeq, @Param("limit") int limit);
-  List<MojipPostDTO> searchMojipList(@Param("previousPostSeq") Long previousPostSeq, @Param("limit") int limit, @Param("searchWord") String keyword);
-  MojipPostDTO selectMojipDetail(@Param("postSeq") Long postSeq);
+  List<MojipPostDetailDTO> selectMojipList(@Param("previousPostSeq") Long previousPostSeq, @Param("limit") int limit);
+  List<MojipPostDetailDTO> searchMojipList(@Param("previousPostSeq") Long previousPostSeq, @Param("limit") int limit, @Param("searchWord") String keyword);
+  MojipPostDetailDTO selectMojipDetail(@Param("postSeq") Long postSeq);
+  int insertMojip(@Param("userSeq") Long userSeq, @Param("title") String title,
+      @Param("applyStartDate") Date applyStartDate, @Param("applyEndDate") Date applyEndDate,
+      @Param("experienceDate") Date experienceDate, @Param("content") String content);
 }
