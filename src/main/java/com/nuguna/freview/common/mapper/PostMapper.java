f7f0b6975a28eb309f8e7b61e181f872b0ccfc01
seungdangdang <yseungun.21@gmail.com>
2024-07-19 09:19:40 +0900

    feat: 특정 공지글의 조회수를 증가시키는 mapper 메서드 생성

--- src/main/java/com/nuguna/freview/common/mapper/PostMapper.java
+++ src/main/java/com/nuguna/freview/common/mapper/PostMapper.java
@@ -1,6 +1,7 @@
 package com.nuguna.freview.common.mapper;
 
 import com.nuguna.freview.common.dto.response.page.NoticeDetailResponseDTO;
+import java.sql.Timestamp;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -9,4 +10,5 @@ public interface PostMapper {
 
   NoticeDetailResponseDTO selectNotice(Long postSeq);
   int checkPostLiked(@Param("userSeq") Long userSeq, @Param("postSeq") Long postSeq);
+  void insertPostViewCount(@Param("postSeq") Long postSeq);
 }
