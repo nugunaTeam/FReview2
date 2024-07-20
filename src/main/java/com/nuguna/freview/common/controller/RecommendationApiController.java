0b06ba6a561b911f351adbdfd6b91a3c8ceb02f9
seungdangdang <yseungun.21@gmail.com>
2024-07-20 13:13:31 +0900

    feat: 체험단 추천 리스트를 가져오는 API 컨트롤러 구현

---
+++ src/main/java/com/nuguna/freview/common/controller/RecommendationApiController.java
@@ -0,0 +1,50 @@
+package com.nuguna.freview.common.controller;
+
+import com.nuguna.freview.common.dto.request.CustomerRecommendationListRequestDTO;
+import com.nuguna.freview.common.dto.response.CustomerRecommendationResponseDTO;
+import com.nuguna.freview.common.dto.response.page.CustomerRecommendationListResponseDTO;
+import com.nuguna.freview.common.service.RecommendationService;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/common/recommendation/customer")
+public class RecommendationApiController {
+
+  private final RecommendationService customerService;
+  private final int PAGE_SIZE = 20;
+
+  @Autowired
+  public RecommendationApiController(RecommendationService customerService) {
+    this.customerService = customerService;
+  }
+
+  @RequestMapping(value = "", method = RequestMethod.POST)
+  public CustomerRecommendationListResponseDTO getRecommendation(
+      @RequestBody CustomerRecommendationListRequestDTO requestDTO) {
+    Long previousUserSeq = requestDTO.getPreviousUserSeq();
+    String searchWord = requestDTO.getSearchWord();
+
+    if (previousUserSeq == null) {
+      previousUserSeq = Long.MAX_VALUE;
+    }
+
+    CustomerRecommendationListResponseDTO responseDTO = new CustomerRecommendationListResponseDTO();
+    try {
+      List<CustomerRecommendationResponseDTO> customerList = customerService.getRecommendationCustomerList(
+          previousUserSeq, PAGE_SIZE);
+      boolean hasMore = customerList.size() == PAGE_SIZE;
+      responseDTO.setCustomerList(customerList);
+      responseDTO.setHasMore(hasMore);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return responseDTO;
+  }
+}
