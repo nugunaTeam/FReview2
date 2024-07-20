023c7e282906240911e4ed7090acff7ca7b38e4d
seungdangdang <yseungun.21@gmail.com>
2024-07-20 13:14:50 +0900

    feat: 체험단 추천 리스트를 가져오는 서비스 메서드 구현

---
+++ src/main/java/com/nuguna/freview/common/service/RecommendationService.java
@@ -0,0 +1,9 @@
+package com.nuguna.freview.common.service;
+
+import com.nuguna.freview.common.dto.response.CustomerRecommendationResponseDTO;
+import java.util.List;
+
+public interface RecommendationService {
+
+  List<CustomerRecommendationResponseDTO> getRecommendationCustomerList(Long previousSeq, int limit);
+}
---
+++ src/main/java/com/nuguna/freview/common/service/impl/RecommendationServiceImpl.java
@@ -0,0 +1,25 @@
+package com.nuguna.freview.common.service.impl;
+
+import com.nuguna.freview.common.dto.response.CustomerRecommendationResponseDTO;
+import com.nuguna.freview.common.mapper.RecommendationMapper;
+import com.nuguna.freview.common.service.RecommendationService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecommendationServiceImpl implements RecommendationService {
+
+  private final RecommendationMapper recommendationMapper;
+
+  @Autowired
+  public RecommendationServiceImpl(RecommendationMapper recommendationMapper) {
+    this.recommendationMapper = recommendationMapper;
+  }
+
+  @Override
+  public List<CustomerRecommendationResponseDTO> getRecommendationCustomerList(Long previousSeq,
+      int limit) {
+    return recommendationMapper.selectCustomer(previousSeq, limit);
+  }
+}
