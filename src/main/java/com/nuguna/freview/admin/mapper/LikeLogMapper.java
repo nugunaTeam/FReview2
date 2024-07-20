0efb54849669137dab4c97d68b17811e6afaf0c9
seungdangdang <yseungun.21@gmail.com>
2024-07-21 00:03:17 +0900

    feat: 좋아요 로그 관련 mybatis 매퍼 파일 구현

--- src/main/java/com/nuguna/freview/admin/mapper/LikeLogMapper.java
+++ src/main/java/com/nuguna/freview/admin/mapper/LikeLogMapper.java
@@ -1,10 +1,13 @@
-package com.nuguna.freview.common.mapper;
+package com.nuguna.freview.admin.mapper;
 
+import com.nuguna.freview.admin.vo.LikeLogVO;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
-public interface LikeMapper {
+public interface LikeLogMapper {
 
   int insertLikeLog(@Param("postSeq") Long postSeq, @Param("userSeq") Long userSeq, @Param("code") String code);
+  List<LikeLogVO> getNewLogs(Long lastProcessedSeq);
 }
