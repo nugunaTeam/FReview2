f266bba9e833e9dea3a94c87ff4029035b210a0c
yeong1203 <cozyluv117@gmail.com>
2024-07-23 01:47:21 +0900

    Feat:스토어 활동 페이지 서비스 수정

--- src/main/java/com/nuguna/freview/store/service/StoreActivityPageService.java
+++ src/main/java/com/nuguna/freview/store/service/StoreActivityPageService.java
@@ -3,12 +3,10 @@ package com.nuguna.freview.store.service;
 import com.nuguna.freview.store.dto.response.StoreActivitySendLikeResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreActivitySendZzimResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreActivityWrittenPostResponseDTO;
-import com.nuguna.freview.store.dto.response.StoreActivityWrittenReplyResponseDTO;
 import java.util.List;
 
 public interface StoreActivityPageService {
     List<StoreActivitySendLikeResponseDTO> storeActivityPageSendLike(Long userSeq);
     List<StoreActivitySendZzimResponseDTO> storeActivityPageSendZzim(Long userSeq);
     List<StoreActivityWrittenPostResponseDTO> storeActivityPageWrittenPost(Long userSeq);
-    List<StoreActivityWrittenReplyResponseDTO> storeActivityPageWrittenReply(Long userSeq);
 }
