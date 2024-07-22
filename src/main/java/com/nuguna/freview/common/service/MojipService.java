712efc73558d0d06d598163c5262277bb691c64f
seungdangdang <yseungun.21@gmail.com>
2024-07-22 12:10:13 +0900

    feat: 모집글 등록 / 삭제 시 포인트 로그가 쌓이는 서비스 로직 추가

--- src/main/java/com/nuguna/freview/common/service/MojipService.java
+++ src/main/java/com/nuguna/freview/common/service/MojipService.java
@@ -11,4 +11,5 @@ public interface MojipService {
   boolean createMojip(Long userSeq, String title, Date applyStartDate, Date applyEndDate, Date experienceDate, String content);
   boolean updateMojip(Long postSeq, String title, String content);
   boolean applyMojip(Long fromUserSeq, Long toUserSeq, Long fromPostSeq);
+  boolean deletePost(Long postSeq);
 }
