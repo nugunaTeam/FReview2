ca4a3464ac0db0160493d4a306ea771e28f31404
haram <rlagkfka9611@naver.com>
2024-07-24 19:07:20 +0900

    feat : 인증 성공 후 jwt 토큰 발급

---
+++ src/main/java/com/nuguna/freview/security/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.nuguna.freview.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Base64;
+import java.util.Date;
+
+public class JwtUtil {
+
+  private String secretKey = "secret";
+  private long tokenValidTime = 1000L * 60 * 60;
+
+  // JWT 토큰 생성
+  public String createToken(Long userSeq, String userEmail, String role) {
+    byte[] decoded = Base64.getDecoder().decode(secretKey);
+    String decodedSecretKey = new String(decoded);
+    Claims claims = Jwts.claims();
+    claims.put("userSeq", userSeq);
+    claims.put("userEmail", userEmail);
+    claims.put("roles", role); // 정보는 key / value 쌍으로 저장된다.
+    Date now = new Date();
+    return Jwts.builder()
+        .setClaims(claims) // 정보 저장
+        .setIssuedAt(now) // 토큰 발행 시간 정보
+        .setExpiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
+        .signWith(SignatureAlgorithm.HS512,secretKey)  // 사용할 암호화 알고리즘과
+        // signature 에 들어갈 secret값 세팅
+        .compact();
+  }
+
+}
--- src/main/java/com/nuguna/freview/security/login/SuccessHandler.java
+++ src/main/java/com/nuguna/freview/security/login/SuccessHandler.java
@@ -1,7 +1,54 @@
 package com.nuguna.freview.security.login;
 
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import com.nuguna.freview.security.JwtUtil;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-public class SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class SuccessHandler implements AuthenticationSuccessHandler {
+
+  JwtUtil jwtUtil;
+
+  SuccessHandler(JwtUtil jwtUtil) {
+    this.jwtUtil = jwtUtil;
+  }
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse, Authentication authentication)
+      throws IOException, ServletException {
+
+    CustomUserDetail customUserDetail = (CustomUserDetail) authentication.getPrincipal();
+
+    Long userSeq = customUserDetail.getUserVO().getSeq();
+    String userEmail = customUserDetail.getUsername();
+    Collection<GrantedAuthority> authorities = customUserDetail.getAuthorities();
+    Iterator<GrantedAuthority> iterator = authorities.iterator();
+    GrantedAuthority grantedAuthority = iterator.next();
+    String role = grantedAuthority.getAuthority();
+
+    String AccessToken = jwtUtil.createToken(userSeq, userEmail, role);
+    String targetUrl = determinTargetUrl(role);
+    httpServletResponse.setHeader("Authorization", "Bearer " + AccessToken);
+    httpServletResponse.sendRedirect(targetUrl);
+  }
+
+  private String determinTargetUrl(String role) {
+    if(role.equals("ROLE_ADMIN")) {
+      return "/admin/manage/store";
+    }else if(role.equals("ROLE_CUSTOMER")) {
+      return "/customer/main-page";
+    } else if(role.equals("ROLE_STORE")) {
+      return "/store/main-page";
+    }
+    return "/error";
+  }
+
 
 }
