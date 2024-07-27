46d7670341bd7f1088aeb18860c31dfa5c627dba
seungdangdang <yseungun.21@gmail.com>
2024-07-27 14:07:26 +0900

    feat: 유저의 누적된 관심 데이터를 얻는 mybatis 매퍼 파일 구현

---
+++ src/main/java/com/nuguna/freview/common/mapper/InterestAccumulationMapper.java
@@ -0,0 +1,10 @@
+package com.nuguna.freview.common.mapper;
+
+import com.nuguna.freview.common.dto.InterestAccumulationDTO;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface InterestAccumulationMapper {
+  List<InterestAccumulationDTO> getByUserSeq(Long userSeq);
+}
