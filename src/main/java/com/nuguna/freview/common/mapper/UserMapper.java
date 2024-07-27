20cefd7f1b492168b171a30ef1fed6edc4d2f682
seungdangdang <yseungun.21@gmail.com>
2024-07-27 14:06:37 +0900

    feat: dish / category 별 유저 리스트, 랜덤한 유저 리스트 얻는 mybatis 매퍼 파일 구현

--- src/main/java/com/nuguna/freview/common/mapper/UserMapper.java
+++ src/main/java/com/nuguna/freview/common/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.nuguna.freview.common.mapper;
 
+import com.nuguna.freview.common.dto.PersonalizedUserDTO;
 import com.nuguna.freview.common.vo.user.UserVO;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -9,4 +11,7 @@ public interface UserMapper {
 
   void insertCustomer(UserVO uservo);
   UserVO selectUser(@Param("userSeq") Long userSeq);
+  List<PersonalizedUserDTO> findByDish(String dish);
+  List<PersonalizedUserDTO> findByCategoryExcludingDish(@Param("category") String category, @Param("dish") String dish);
+  List<PersonalizedUserDTO> findRandomUsers(@Param("limit") int limit);
 }
