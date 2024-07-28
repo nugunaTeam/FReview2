c68405f7558f78219d7c1b25275ab3858b07d7a2
yeong1203 <cozyluv117@gmail.com>
2024-07-29 02:09:18 +0900

    Feat:스토어 알림 페이지 mybatis mapper 수정

--- src/main/java/com/nuguna/freview/store/mapper/StoreNotificationPageMapper.java
+++ src/main/java/com/nuguna/freview/store/mapper/StoreNotificationPageMapper.java
@@ -1,8 +1,10 @@
 package com.nuguna.freview.store.mapper;
 
-import com.nuguna.freview.store.dto.response.StoreNotificationExperienceResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationExperienceApplyResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationExperienceProposeResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreNotificationReceivedLikeResponseDTO;
-import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimCustomerResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimStoreResponseDTO;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -11,7 +13,8 @@ import org.apache.ibatis.annotations.Param;
 public interface StoreNotificationPageMapper {
   String storeNotificationPage(@Param("userSeq") Long userSeq);
   List<StoreNotificationReceivedLikeResponseDTO> storeNotificationReceivedLike(Long userSeq);
-  List<StoreNotificationReceivedZzimResponseDTO> storeNotificationReceivedZzim(Long userSeq);
-  List<StoreNotificationExperienceResponseDTO> storeNotificationExperience(Long userSeq);
-
+  List<StoreNotificationReceivedZzimCustomerResponseDTO> storeNotificationReceivedZzimCustomer(Long userSeq);
+  List<StoreNotificationReceivedZzimStoreResponseDTO> storeNotificationReceivedZzimStore(Long userSeq);
+  List<StoreNotificationExperienceApplyResponseDTO> storeNotificationExperienceApply(Long userSeq);
+  List<StoreNotificationExperienceProposeResponseDTO> storeNotificationExperiencePropose(Long userSeq);
 }
