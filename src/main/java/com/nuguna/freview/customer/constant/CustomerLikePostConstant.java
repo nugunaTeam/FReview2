868092b8a23506790cb30fbed4fc846b48c1ac68
wonjun <wonjon0930@daum.net>
2024-08-01 03:51:05 +0900

    feat: 공통된 페이지네이션 기능 Util 클래스화 완료

--- src/main/java/com/nuguna/freview/customer/constant/CustomerLikePostConstant.java
+++ src/main/java/com/nuguna/freview/customer/constant/CustomerLikePostConstant.java
@@ -2,6 +2,6 @@ package com.nuguna.freview.customer.constant;
 
 public class CustomerLikePostConstant {
 
-  public static final int CUSTOMER_LIKE_POST_SIZE = 5;
+  public static final int CUSTOMER_LIKE_POST_PAGE_SIZE = 5;
   public static final int CUSTOMER_LIKE_POST_PAGE_BLOCK_SIZE = 5;
 }
---
+++ src/main/java/com/nuguna/freview/global/exception/IllegalPageAccessException.java
@@ -0,0 +1,11 @@
+package com.nuguna.freview.global.exception;
+
+public class IllegalPageAccessException extends RuntimeException {
+
+  public IllegalPageAccessException() {
+  }
+
+  public IllegalPageAccessException(String message) {
+    super(message);
+  }
+}
