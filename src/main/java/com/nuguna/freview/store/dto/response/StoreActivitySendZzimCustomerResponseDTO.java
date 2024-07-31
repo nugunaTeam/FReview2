37e7a2c3dfb5f8437a7676dc323dd3a5657bfe0c
yeong1203 <cozyluv117@gmail.com>
2024-07-31 20:55:05 +0900

    feat: 활동 페이지 request response DTO 수정

--- src/main/java/com/nuguna/freview/store/dto/response/StoreActivitySendZzimCustomerResponseDTO.java
+++ src/main/java/com/nuguna/freview/store/dto/response/StoreActivitySendZzimCustomerResponseDTO.java
@@ -1,4 +1,4 @@
-package com.nuguna.freview.store.dto.request.page;
+package com.nuguna.freview.store.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,7 +7,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class StoreActivityPageRequestDTO {
+public class StoreActivitySendZzimCustomerResponseDTO {
   private Long userSeq;
+  private Long zzimUserSeq;
+  private String nickname;
   private String code;
+  private String foodTypes;
 }
--- src/main/java/com/nuguna/freview/store/dto/response/StoreActivitySendZzimResponseDTO.java
+++ src/main/java/com/nuguna/freview/store/dto/response/StoreActivitySendZzimResponseDTO.java
@@ -1,7 +1,5 @@
 package com.nuguna.freview.store.dto.response;
 
-import com.nuguna.freview.common.vo.user.foodType.FoodType;
-import java.sql.Timestamp;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,13 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoreActivitySendZzimResponseDTO {
-  private Long userSeq;
-  private Long toUserSeq;
-  private String nickname;
-  private String code;
-  private String storeLocation;
-  private Long zzimCount;
-  private List<FoodType> foodTypeList;
-  private Timestamp createdAt;
+
+  List<StoreActivitySendZzimCustomerResponseDTO> customerZzimList;
+  List<StoreActivitySendZzimStoreResponseDTO> storeZzimList;
 
 }
