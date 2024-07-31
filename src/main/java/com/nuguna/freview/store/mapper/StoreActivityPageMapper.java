12ac192bd296324882391e737519703031f28205
yeong1203 <cozyluv117@gmail.com>
2024-07-31 20:54:25 +0900

    feat: 활동 페이지 mybatis mapper 수정

--- src/main/java/com/nuguna/freview/store/mapper/StoreActivityPageMapper.java
+++ src/main/java/com/nuguna/freview/store/mapper/StoreActivityPageMapper.java
@@ -1,7 +1,8 @@
 package com.nuguna.freview.store.mapper;
 
 import com.nuguna.freview.store.dto.response.StoreActivitySendLikeResponseDTO;
-import com.nuguna.freview.store.dto.response.StoreActivitySendZzimResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreActivitySendZzimCustomerResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreActivitySendZzimStoreResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreActivityWrittenPostResponseDTO;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,6 +12,7 @@ import org.apache.ibatis.annotations.Param;
 public interface StoreActivityPageMapper {
   Long storeActivityPage(@Param("userSeq") Long userSeq);
   List<StoreActivitySendLikeResponseDTO> storeActivityPageSendLike(Long userSeq);
-  List<StoreActivitySendZzimResponseDTO> storeActivityPageSendZzim(Long userSeq);
+  List<StoreActivitySendZzimStoreResponseDTO> storeActivitySendZzimStore(Long userSeq);
+  List<StoreActivitySendZzimCustomerResponseDTO> storeActivitySendZzimCustomer(Long userSeq);
   List<StoreActivityWrittenPostResponseDTO> storeActivityPageWrittenPost(Long userSeq);
 }
