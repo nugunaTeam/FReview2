dd29c3e31d567115491b889352c58a86ccdbd8d3
seungdangdang <yseungun.21@gmail.com>
2024-08-03 13:49:27 +0900

    feat: 완료된 체험 리스트를 가져오는 mybatis 매퍼 파일 구현

--- src/main/java/com/nuguna/freview/admin/mapper/DoneExperienceAccumulationMapper.java
+++ src/main/java/com/nuguna/freview/admin/mapper/DoneExperienceAccumulationMapper.java
@@ -2,6 +2,7 @@ package com.nuguna.freview.admin.mapper;
 
 import com.nuguna.freview.admin.dto.DoneExperienceDTO;
 import java.time.LocalDate;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
@@ -10,4 +11,5 @@ public interface DoneExperienceAccumulationMapper {
   DoneExperienceDTO findByDate(LocalDate date);
   void insert(DoneExperienceDTO record);
   void update(DoneExperienceDTO record);
+  List<DoneExperienceDTO> findAll();
 }
