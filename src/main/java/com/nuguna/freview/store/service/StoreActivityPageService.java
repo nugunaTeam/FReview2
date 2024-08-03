78b90e014aa1e1bf39044114dacf944903b2039e
yeong1203 <cozyluv117@gmail.com>
2024-08-03 11:35:02 +0900

    feat: 활동 페이지 내가 찜한 체험단 서비스 수정

--- src/main/java/com/nuguna/freview/store/service/StoreActivityPageService.java
+++ src/main/java/com/nuguna/freview/store/service/StoreActivityPageService.java
@@ -1,5 +1,6 @@
 package com.nuguna.freview.store.service;
 
+import com.nuguna.freview.global.exception.IllegalPageAccessException;
 import com.nuguna.freview.store.dto.response.StoreActivitySendLikeResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreActivitySendZzimCustomerResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreActivitySendZzimStoreResponseDTO;
@@ -12,6 +13,7 @@ public interface StoreActivityPageService {
 //    StorePaginationIfoResponseDTO storePaginationInfo(Long userSeq, StoreListPaginationResponseDTO storeListPaginationResponseDTO);
     List<StoreActivitySendLikeResponseDTO> storeActivityPageSendLike(Long userSeq, StoreListPaginationResponseDTO storeListPaginationResponseDTO);
     List<StoreActivitySendZzimStoreResponseDTO> storeActivitySendZzimStore(Long userSeq, StoreListPaginationResponseDTO storeListPaginationResponseDTO);
-    List<StoreActivitySendZzimCustomerResponseDTO> storeActivitySendZzimCustomer(Long userSeq, StoreListPaginationResponseDTO storeListPaginationResponseDTO);
+    StoreActivitySendZzimCustomerResponseDTO storeActivitySendZzimCustomer(Long userSeq, int targetPage)
+        throws IllegalPageAccessException;
     List<StoreActivityWrittenPostResponseDTO> storeActivityPageWrittenPost(Long userSeq, StoreListPaginationResponseDTO storeListPaginationResponseDTO);
 }
--- src/main/java/com/nuguna/freview/store/service/impl/StoreActivityPageServiceImpl.java
+++ src/main/java/com/nuguna/freview/store/service/impl/StoreActivityPageServiceImpl.java
@@ -6,6 +6,7 @@ import static com.nuguna.freview.store.constant.StoreListConstant.STORE_LIST_PAG
 import com.nuguna.freview.customer.dto.response.PaginationInfoResponseDTO;
 import com.nuguna.freview.global.exception.IllegalPageAccessException;
 import com.nuguna.freview.global.util.PaginationUtil;
+import com.nuguna.freview.store.dto.response.SendZzimCustomerInfoDTO;
 import com.nuguna.freview.store.dto.response.StoreActivitySendLikeResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreActivitySendZzimCustomerResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreActivitySendZzimStoreResponseDTO;
@@ -30,12 +31,6 @@ public class StoreActivityPageServiceImpl implements StoreActivityPageService {
   public StoreActivityPageServiceImpl(StoreActivityPageMapper storeActivityPageMapper) {
     this.storeActivityPageMapper = storeActivityPageMapper;
   }
-  // 수정전
-  // 좋아요한 글
-//  @Override
-//  public List<StoreActivitySendLikeResponseDTO> storeActivityPageSendLike(Long userSeq){
-//    return storeActivityPageMapper.storeActivityPageSendLike(userSeq);
-//  }
 
   @Override
   public List<StoreActivitySendLikeResponseDTO> storeActivityPageSendLike(Long userSeq, StoreListPaginationResponseDTO storeListPaginationResponseDTO) {
@@ -59,26 +54,20 @@ public class StoreActivityPageServiceImpl implements StoreActivityPageService {
 
   // 찜한 체험단
   @Override
-  public List<StoreActivitySendZzimCustomerResponseDTO> storeActivitySendZzimCustomer(
-                    Long userSeq, StoreListPaginationResponseDTO storeListPaginationResponseDTO) {
-    log.info("userSeq는?"+userSeq);
-    Integer targetPage = storeListPaginationResponseDTO.getTargetPage();
-//    log.info("targetPage number : "+targetPage);
-    if (targetPage == null || targetPage < 1) {
-      targetPage = 1;
-    }
+  public StoreActivitySendZzimCustomerResponseDTO storeActivitySendZzimCustomer(
+                    Long userSeq, int targetPage) throws IllegalPageAccessException {
 
-    Integer likeListCount = storeActivityPageMapper.storeActivitySendZzimCustomerCount(userSeq);
-    try {
-      PaginationInfoResponseDTO paginationInfo = PaginationUtil.makePaginationViewInfo(
-          targetPage, likeListCount, STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE
-      );
-      return storeActivityPageMapper.storeActivitySendZzimCustomer(userSeq, paginationInfo.getStartPage() - 1, paginationInfo.getEndPage() - paginationInfo.getStartPage() + 1);
+    int likeListCount = storeActivityPageMapper.storeActivitySendZzimCustomerCount(userSeq);
+    log.info("likeListCount = {}", likeListCount);
+    PaginationInfoResponseDTO paginationInfo = PaginationUtil.makePaginationViewInfo(
+          targetPage, likeListCount, STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE);
 
-    } catch (IllegalPageAccessException e) {
-//      log.error("zzim customer error : {}", e.getMessage());
-      throw new RuntimeException(e);
-    }
+    log.info("paginationInfoResponseDTO = {}", paginationInfo);
+
+    List<SendZzimCustomerInfoDTO> sendZzimCustomerInfos = storeActivityPageMapper.storeActivitySendZzimCustomer(
+        userSeq, (targetPage - 1) * STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_SIZE);
+
+    return new StoreActivitySendZzimCustomerResponseDTO(sendZzimCustomerInfos, paginationInfo);
   }
 
   // 찜한 스토어
@@ -95,12 +84,17 @@ public class StoreActivityPageServiceImpl implements StoreActivityPageService {
           targetPage, likeListCount, STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE
       );
       log.info("zzimStore paginationInfo : {}", paginationInfo);
-      return storeActivityPageMapper.storeActivitySendZzimStore(userSeq,(targetPage - 1) * STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE);
+      return storeActivityPageMapper.storeActivitySendZzimStore(userSeq, paginationInfo.getTargetPage(), STORE_LIST_PAGE_BLOCK_SIZE);
 
     } catch (IllegalPageAccessException e) {
-      log.error("zzim store error : {}", e.getMessage());
       throw new RuntimeException(e);
     }
+
+
+//    } catch (IllegalPageAccessException e) {
+//      log.error("zzim store error : {}", e.getMessage());
+//      throw new RuntimeException(e);
+//    }
   }
 
   // 작성한 글
