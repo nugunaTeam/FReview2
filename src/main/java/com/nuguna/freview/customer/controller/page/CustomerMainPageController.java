8899d2d522fc44ddc4114f030ec75c8f0944de4a
haram <rlagkfka9611@naver.com>
2024-08-03 18:15:28 +0900

    feat : 컨트롤러 값 확인 로그

--- src/main/java/com/nuguna/freview/customer/controller/page/CustomerMainPageController.java
+++ src/main/java/com/nuguna/freview/customer/controller/page/CustomerMainPageController.java
@@ -1,9 +1,13 @@
 package com.nuguna.freview.customer.controller.page;
 
+import com.nuguna.freview.common.vo.user.UserVO;
 import com.nuguna.freview.customer.dto.response.page.CustomerMyBrandPageInfoResponseDTO;
 import com.nuguna.freview.customer.service.CustomerPageService;
+import com.nuguna.freview.security.login.CustomUserDetail;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +19,10 @@ public class CustomerMainPageController {
 
   @RequestMapping("/customer/main-page")
   public String customerMyBrandPage(Model model) {
-
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    CustomUserDetail customUserDetail = (CustomUserDetail) authentication.getPrincipal();
+    UserVO userVO = customUserDetail.getUserVO();
+    log.info(userVO.toString());
     return "customer_main";
   }
 
