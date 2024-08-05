94afa5aa59015744ddcf87582a0a04e2aafb2daa
haram <rlagkfka9611@naver.com>
2024-08-05 16:34:26 +0900

    feat : customer 회원 로그인후 메인페이지 이동 컨트롤러 메서드 생성

--- src/main/java/com/nuguna/freview/customer/controller/page/CustomerMainPageController.java
+++ src/main/java/com/nuguna/freview/customer/controller/page/CustomerMainPageController.java
@@ -1,29 +1,24 @@
 package com.nuguna.freview.customer.controller.page;
 
 import com.nuguna.freview.common.vo.user.UserVO;
-import com.nuguna.freview.customer.dto.response.page.CustomerMyBrandPageInfoResponseDTO;
-import com.nuguna.freview.customer.service.CustomerPageService;
-import com.nuguna.freview.security.login.CustomUserDetail;
+import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
 public class CustomerMainPageController {
 
   @RequestMapping("/customer/main-page")
-  public String customerMyBrandPage(Model model) {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    CustomUserDetail customUserDetail = (CustomUserDetail) authentication.getPrincipal();
-    UserVO userVO = customUserDetail.getUserVO();
-    log.info(userVO.toString());
-    return "customer_main";
+  public String customerMyBrandPage(Model model, HttpServletRequest request) {
+    UserVO uvo = (UserVO) request.getAttribute("UserVO");
+
+      log.info(uvo.toString());
+
+      model.addAttribute("userVO", uvo);
+      return "customer_main";
   }
 
 }
