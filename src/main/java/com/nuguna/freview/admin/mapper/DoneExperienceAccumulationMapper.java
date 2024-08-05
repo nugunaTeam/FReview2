0898dfe918a41e2c2ad23b17eb194e6d4b78abd2
seungdangdang <yseungun.21@gmail.com>
2024-08-05 15:49:20 +0900

    feat: 지정된 날짜 사이의 체험 완료 데이터를 얻는 mybatis 매퍼 파일 수정

--- src/main/java/com/nuguna/freview/admin/mapper/DoneExperienceAccumulationMapper.java
+++ src/main/java/com/nuguna/freview/admin/mapper/DoneExperienceAccumulationMapper.java
@@ -4,6 +4,7 @@ import com.nuguna.freview.admin.dto.DoneExperienceDTO;
 import java.time.LocalDate;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface DoneExperienceAccumulationMapper {
@@ -11,5 +12,5 @@ public interface DoneExperienceAccumulationMapper {
   DoneExperienceDTO findByDate(LocalDate date);
   void insert(DoneExperienceDTO record);
   void update(DoneExperienceDTO record);
-  List<DoneExperienceDTO> findAll();
+  List<DoneExperienceDTO> findByDateBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }
