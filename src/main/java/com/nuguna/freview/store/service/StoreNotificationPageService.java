3738e88a1544f89ce24f8de29ebd9e717fabc2f3
yeong1203 <cozyluv117@gmail.com>
2024-08-05 18:06:06 +0900

    feat: 알림 페이지 서비스 구현

--- src/main/java/com/nuguna/freview/store/service/StoreNotificationPageService.java
+++ src/main/java/com/nuguna/freview/store/service/StoreNotificationPageService.java
@@ -1,16 +1,15 @@
 package com.nuguna.freview.store.service;
 
 import com.nuguna.freview.store.dto.response.StoreNotificationExperienceApplyResponseDTO;
-import com.nuguna.freview.store.dto.response.StoreNotificationExperienceProposeResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationExperienceProposalResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreNotificationReceivedLikeResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimCustomerResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimStoreResponseDTO;
-import java.util.List;
 
 public interface StoreNotificationPageService {
-  List<StoreNotificationReceivedLikeResponseDTO> storeNotificationReceivedLike(Long userSeq);
-  List<StoreNotificationReceivedZzimCustomerResponseDTO> storeNotificationReceivedZzimCustomer(Long userSeq);
-  List<StoreNotificationReceivedZzimStoreResponseDTO> storeNotificationReceivedZzimStore(Long userSeq);
-  List<StoreNotificationExperienceApplyResponseDTO> storeNotificationExperienceApply(Long userSeq);
-  List<StoreNotificationExperienceProposeResponseDTO> storeNotificationExperiencePropose(Long userSeq);
+  StoreNotificationReceivedLikeResponseDTO storeNotificationReceivedLike(Long userSeq, int currentPage);
+  StoreNotificationReceivedZzimCustomerResponseDTO storeNotificationReceivedZzimCustomer(Long userSeq, int currentPage);
+  StoreNotificationReceivedZzimStoreResponseDTO storeNotificationReceivedZzimStore(Long userSeq, int currentPage);
+  StoreNotificationExperienceApplyResponseDTO storeNotificationExperienceApply(Long userSeq, int currentPage);
+  StoreNotificationExperienceProposalResponseDTO storeNotificationExperienceProposal(Long userSeq, int currentPage);
 }
--- src/main/java/com/nuguna/freview/store/service/impl/StoreNotificationPageServiceImpl.java
+++ src/main/java/com/nuguna/freview/store/service/impl/StoreNotificationPageServiceImpl.java
@@ -1,7 +1,17 @@
 package com.nuguna.freview.store.service.impl;
 
+import static com.nuguna.freview.store.constant.StoreListConstant.STORE_LIST_PAGE_BLOCK_SIZE;
+import static com.nuguna.freview.store.constant.StoreListConstant.STORE_LIST_PAGE_SIZE;
+
+import com.nuguna.freview.customer.dto.response.PaginationInfoResponseDTO;
+import com.nuguna.freview.global.util.PaginationUtil;
+import com.nuguna.freview.store.dto.response.ExperienceApplyInfoDTO;
+import com.nuguna.freview.store.dto.response.ExperienceProposalInfoDTO;
+import com.nuguna.freview.store.dto.response.ReceivedLikeInfoDTO;
+import com.nuguna.freview.store.dto.response.ReceivedZzimCustomerInfoDTO;
+import com.nuguna.freview.store.dto.response.ReceivedZzimStoreInfoDTO;
 import com.nuguna.freview.store.dto.response.StoreNotificationExperienceApplyResponseDTO;
-import com.nuguna.freview.store.dto.response.StoreNotificationExperienceProposeResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationExperienceProposalResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreNotificationReceivedLikeResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimCustomerResponseDTO;
 import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimStoreResponseDTO;
@@ -17,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class StoreNotificationPageServiceImpl implements StoreNotificationPageService {
+
   private final StoreNotificationPageMapper storeNotificationPageMapper;
 
   @Autowired
@@ -25,28 +36,72 @@ public class StoreNotificationPageServiceImpl implements StoreNotificationPageSe
   }
 
   @Override
-  public List<StoreNotificationReceivedLikeResponseDTO> storeNotificationReceivedLike(Long userSeq){
-    return storeNotificationPageMapper.storeNotificationReceivedLike(userSeq);
+  public StoreNotificationReceivedLikeResponseDTO storeNotificationReceivedLike(Long userSeq,
+      int currentPage) {
+      Integer receivedLikeCount = storeNotificationPageMapper.storeNotificationPageReceivedLikeCount(
+          userSeq);
+    PaginationInfoResponseDTO paginationInfo = PaginationUtil.makePaginationViewInfo(
+      currentPage, receivedLikeCount, STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE
+    );
+
+    List<ReceivedLikeInfoDTO> receivedLikeInfo = storeNotificationPageMapper.storeNotificationReceivedLike(
+        userSeq, (currentPage -1) * STORE_LIST_PAGE_SIZE , STORE_LIST_PAGE_SIZE
+    );
+
+    return new StoreNotificationReceivedLikeResponseDTO(receivedLikeInfo, paginationInfo);
   }
 
   @Override
-  public List<StoreNotificationReceivedZzimCustomerResponseDTO> storeNotificationReceivedZzimCustomer(Long userSeq){
-    return storeNotificationPageMapper.storeNotificationReceivedZzimCustomer(userSeq);
+  public StoreNotificationReceivedZzimCustomerResponseDTO storeNotificationReceivedZzimCustomer(
+      Long userSeq, int currentPage) {
+    int receivedZzimCustomerCount = storeNotificationPageMapper.storeNotificationPageReceivedZzimCustomerCount(userSeq);
+
+    PaginationInfoResponseDTO paginationInfos = PaginationUtil.makePaginationViewInfo(
+        currentPage, receivedZzimCustomerCount, STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE
+    );
+    List<ReceivedZzimCustomerInfoDTO> receivedZzimCustomerInfo = storeNotificationPageMapper.storeNotificationReceivedZzimCustomer(
+        userSeq, (currentPage -1) * STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_SIZE
+    );
+    return new StoreNotificationReceivedZzimCustomerResponseDTO(receivedZzimCustomerInfo, paginationInfos);
   }
 
   @Override
-  public List<StoreNotificationReceivedZzimStoreResponseDTO> storeNotificationReceivedZzimStore(Long userSeq){
-    return storeNotificationPageMapper.storeNotificationReceivedZzimStore(userSeq);
+  public StoreNotificationReceivedZzimStoreResponseDTO storeNotificationReceivedZzimStore(Long userSeq,
+      int currentPage) {
+    int receivedZzimStoreCount = storeNotificationPageMapper.storeNotificationPageReceivedZzimStoreCount(userSeq);
+    PaginationInfoResponseDTO paginationInfos = PaginationUtil.makePaginationViewInfo(
+        currentPage, receivedZzimStoreCount, STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE
+    );
+    List<ReceivedZzimStoreInfoDTO> receivedZzimStoreInfo = storeNotificationPageMapper.storeNotificationReceivedZzimStore(
+        userSeq, (currentPage -1) * STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_SIZE
+    );
+    return new StoreNotificationReceivedZzimStoreResponseDTO(receivedZzimStoreInfo, paginationInfos);
   }
 
   @Override
-  public List<StoreNotificationExperienceApplyResponseDTO> storeNotificationExperienceApply(Long userSeq){
-    return storeNotificationPageMapper.storeNotificationExperienceApply(userSeq);
+  public StoreNotificationExperienceApplyResponseDTO storeNotificationExperienceApply(Long userSeq,
+      int currentPage) {
+    int experienceApplyCount = storeNotificationPageMapper.storeNotificationPageActivityApplyCount(userSeq);
+    PaginationInfoResponseDTO paginationInfos = PaginationUtil.makePaginationViewInfo(
+        currentPage, experienceApplyCount, STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE
+    );
+    List<ExperienceApplyInfoDTO> experienceApplyInfo =  storeNotificationPageMapper.storeNotificationExperienceApply(
+        userSeq, (currentPage -1) * STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_SIZE
+    );
+    return new StoreNotificationExperienceApplyResponseDTO(experienceApplyInfo, paginationInfos);
   }
 
   @Override
-  public List<StoreNotificationExperienceProposeResponseDTO> storeNotificationExperiencePropose(Long userSeq){
-    return storeNotificationPageMapper.storeNotificationExperiencePropose(userSeq);
+  public StoreNotificationExperienceProposalResponseDTO storeNotificationExperienceProposal(Long userSeq,
+      int currentPage) {
+    int experienceProposalCount = storeNotificationPageMapper.storeNotificationPageActivityProposalCount(userSeq);
+    PaginationInfoResponseDTO paginationInfos = PaginationUtil.makePaginationViewInfo(
+      currentPage, experienceProposalCount, STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_BLOCK_SIZE
+    );
+    List<ExperienceProposalInfoDTO> experienceProposalInfo = storeNotificationPageMapper.storeNotificationExperienceProposal(
+        userSeq, (currentPage -1)*STORE_LIST_PAGE_SIZE, STORE_LIST_PAGE_SIZE
+    );
+    return new StoreNotificationExperienceProposalResponseDTO( experienceProposalInfo, paginationInfos);
   }
 
 }
