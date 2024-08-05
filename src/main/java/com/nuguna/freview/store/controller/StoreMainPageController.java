97ac10674d7eb731094d33842e5a4ff932a80965
haram <rlagkfka9611@naver.com>
2024-08-05 16:36:21 +0900

    feat : Store 회원 로그인후 메인페이지 이동 컨트롤러 메서드 생성

--- src/main/java/com/nuguna/freview/store/controller/StoreMainPageController.java
+++ src/main/java/com/nuguna/freview/store/controller/StoreMainPageController.java
@@ -1,8 +1,9 @@
 package com.nuguna.freview.store.controller;
 
-import com.nuguna.freview.customer.service.CustomerPageService;
+import com.nuguna.freview.common.vo.user.UserVO;
+import com.nuguna.freview.security.login.CustomUserDetail;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
