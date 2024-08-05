b019bd933c9ff2f7f19a85136ba6544b7983d784
haram <rlagkfka9611@naver.com>
2024-08-05 16:37:51 +0900

    feat : accessToken만 없으면 토큰 재생성, refreshToken까지 없으면 로그아웃, 토큰 확인 후 uvo 전달

--- src/main/java/com/nuguna/freview/security/jwtfilter/JWTfilter.java
+++ src/main/java/com/nuguna/freview/security/jwtfilter/JWTfilter.java
@@ -3,22 +3,13 @@ package com.nuguna.freview.security.jwtfilter;
 import com.nuguna.freview.common.vo.user.UserVO;
 import com.nuguna.freview.security.JwtUtil;
 import com.nuguna.freview.security.jwtfilter.service.JwtUserService;
-import com.nuguna.freview.security.login.CustomUserDetail;
-import com.nuguna.freview.security.login.service.LoginService;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
@@ -41,25 +32,68 @@ public class JWTfilter extends OncePerRequestFilter {
     Cookie[] cookies = httpServletRequest.getCookies();
     String accessToken = null;
     String refreshToken = null;
+
     for(Cookie c : cookies) {
-      if(c.getName().equals("refresh")) {
-        refreshToken = c.getValue();
-      }
       if(c.getName().equals("access")) {
         accessToken = c.getValue();
       }
+      if(c.getName().equals("refresh")) {
+        refreshToken = c.getValue();
+      }
     }
 
+    // accessToken만 없으면 토큰 재생성, refreshToken까지 없으면 로그아웃
+    if(accessToken == null) {
+
+      Cookie[] cookiess = httpServletRequest.getCookies();
+      for(Cookie c : cookiess) {
+        if(c.getName().equals("refresh")) {
+          c.setValue("");
+          c.setPath("/");
+          c.setMaxAge(0); // 쿠키 만료 설정
+          httpServletResponse.addCookie(c);
+        }
+      }
+
+      Cookie accessCookie = new Cookie("access", refreshToken);
+      accessCookie.setPath("/");
+      accessCookie.setMaxAge(10);
+      accessCookie.setSecure(true);
+      httpServletResponse.addCookie(accessCookie);
+
+      Cookie refreshCookie = new Cookie("refresh", refreshToken);
+      refreshCookie.setPath("/");
+      refreshCookie.setMaxAge(60*60);
+      refreshCookie.setSecure(true);
+      httpServletResponse.addCookie(refreshCookie);
+
+      accessToken = refreshToken;
+    }
+
+
+    if(refreshToken == null) {
+      httpServletResponse.sendRedirect("/expired-token");
+      return;
+    }
+
+    // 토큰 값으로 UserVO 만들기
     String role = jwtutl.getRole(accessToken);
     String email = jwtutl.getUserEmail(accessToken);
+    Long seq = jwtutl.getUserSeq(accessToken);
+    String businessNumber = jwtutl.getBusinessNumber(accessToken);
+    String code = jwtutl.getCode(accessToken);
+    String nickname = jwtutl.getNickname(accessToken);
+    String profile = jwtutl.getProfilePhotoUrl(accessToken);
+    String introduce = jwtutl.getIntroduce(accessToken);
+    String subEmail = jwtutl.getSubEmail(accessToken);
+    String loginType = jwtutl.getLoginType(accessToken);
+    boolean isWithDrawn = jwtutl.getIsWithDrawn(accessToken);
+    String ageGroup = jwtutl.getAgeGroup(accessToken);
+    String storeLocation = jwtutl.getStoreLocation(accessToken);
 
-    GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role);
-    List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
-    grantedAuthorities.add(grantedAuthority);
+    UserVO uvo = new UserVO(seq,businessNumber,code,email,null,nickname,profile,introduce,subEmail,loginType,isWithDrawn,ageGroup,storeLocation,null,null);
 
-    UserVO uvo = jwtUserService.searchUserByID(email);
-    CustomUserDetail customUserDetail = new CustomUserDetail(email,"password",grantedAuthorities,uvo);
-    SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(customUserDetail,null,grantedAuthorities));
+    httpServletRequest.setAttribute("UserVO", uvo);
     filterChain.doFilter(httpServletRequest, httpServletResponse);
   }
 }
