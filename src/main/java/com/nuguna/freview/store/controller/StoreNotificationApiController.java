08d31840f7d1a8fdca1eee52af7d169affec1372
yeong1203 <167217894+yeong1203@users.noreply.github.com>
2024-08-06 14:07:18 +0900

    [feat-store] 스토어 알림페이지 > 좋아요 | 찜 | 체험 탭 구현 (#36)
    * Feat:스토어를 좋아요/찜 한 유저 리스트 & 작성한 글에 댓글 리스트 &  지원자/제안자 리스트 컨트롤러 구현
    
    * Feat:스토어를 좋아요/찜 한 유저 리스트 & 작성한 글에 댓글 리스트 &  지원자/제안자 리스트 서비스 구현
    
    * Feat:스토어를 좋아요/찜 한 유저 리스트 & 작성한 글에 댓글 리스트 &  지원자/제안자 리스트 mybatis mapper 구현
    
    * Feat:스토어를 좋아요/찜한 유저 리스트 & 지원자/제안자 리스트 컨트롤러 수정
    
    * Feat:스토어를 좋아요/찜한 유저 리스트 & 지원자/제안자 리스트 서비스 수정
    
    * Feat:스토어를 좋아요/찜한 유저 리스트 & 지원자/제안자 리스트 mybatis mapper 수정
    
    * Feat:스토어 알림 페이지 jsp 구현(진행중)
    
    * Feat:스토어 알림 - 좋아요/찜/체험 페이지 DTO 수정 및 추가
    
    * Feat:스토어 알림 페이지 Controller 수정
    
    * Feat:스토어 알림 페이지 mybatis mapper 수정
    
    * Feat:스토어 알림 페이지 서비스 수정
    
    * Feat:스토어 알림 페이지 jsp 수정
    
    * feat: 알림 페이지 서비스 수정
    
    * feat: 알림 페이지 컨트롤러 수정
    
    * feat: 알림 페이지 컨트롤러 수정
    
    * feat: 알림 페이지 jsp 경로 수정
    
    * feat: 알림 페이지 mapper DTO 구현
    
    * feat: 알림 페이지 서비스 구현
    
    * feat: 알림 페이지 컨트롤러 구현
    
    * feat: 알림 페이지 mybatis mapping 추가 구현
    
    * feat: 알림 페이지 mybatis mapping 구현
    
    * feat: 알림 페이지 DTO 수정
    
    * feat: 알림 페이지 DTO 수정
    
    * chore: 불필요한 파일 삭제
    
    * feat: 알림 페이지 서비스 수정
    
    * feat: 알림 페이지 api 컨트롤러 수정
    
    * feat: 알림 페이지 mybatis & DTO 수정
    
    * feat: 알림 페이지 mybatis 수정
    
    * feat: 알림 페이지 jsp 수정

---
+++ src/main/java/com/nuguna/freview/store/controller/StoreNotificationApiController.java
@@ -0,0 +1,58 @@
+package com.nuguna.freview.store.controller;
+
+import com.nuguna.freview.global.exception.IllegalPageAccessException;
+import com.nuguna.freview.store.dto.response.StoreNotificationExperienceApplyResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationExperienceProposalResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationReceivedLikeResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimCustomerResponseDTO;
+import com.nuguna.freview.store.dto.response.StoreNotificationReceivedZzimStoreResponseDTO;
+import com.nuguna.freview.store.service.StoreNotificationPageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/store/notification")
+public class StoreNotificationApiController {
+  private final StoreNotificationPageService storeNotificationPageService;
+
+  @Autowired
+  public StoreNotificationApiController(StoreNotificationPageService storeNotificationPageService) {
+    this.storeNotificationPageService = storeNotificationPageService;
+  }
+
+  @RequestMapping(value = "/received-like", method = RequestMethod.GET)
+  public StoreNotificationReceivedLikeResponseDTO storeNotificationReceivedLike(
+      @RequestParam("userSeq") Long userSeq, @RequestParam Integer currentPage) throws IllegalPageAccessException {
+    return storeNotificationPageService.storeNotificationReceivedLike(userSeq, currentPage);
+  }
+
+  @RequestMapping(value = "/received-zzim-customer", method = RequestMethod.GET)
+  public StoreNotificationReceivedZzimCustomerResponseDTO storeNotificationReceivedZzimCustomer (
+      @RequestParam("userSeq") Long userSeq, @RequestParam Integer currentPage) throws IllegalPageAccessException {
+    return storeNotificationPageService.storeNotificationReceivedZzimCustomer(userSeq, currentPage);
+  }
+
+  @RequestMapping(value = "/received-zzim-store", method = RequestMethod.GET)
+  public StoreNotificationReceivedZzimStoreResponseDTO storeNotificationReceivedZzimStore (
+      @RequestParam("userSeq") Long userSeq, @RequestParam Integer currentPage) throws IllegalPageAccessException {
+      return storeNotificationPageService.storeNotificationReceivedZzimStore(userSeq, currentPage);
+  }
+
+  @RequestMapping(value = "/experience-apply", method = RequestMethod.GET)
+  public StoreNotificationExperienceApplyResponseDTO storeNotificationExperienceApply(
+      @RequestParam("userSeq") Long userSeq, @RequestParam Integer currentPage) {
+      return storeNotificationPageService.storeNotificationExperienceApply(userSeq, currentPage);
+  }
+
+  @RequestMapping(value = "/experience-proposal", method = RequestMethod.GET)
+  public StoreNotificationExperienceProposalResponseDTO storeNotificationExperienceProposal(
+      @RequestParam("userSeq") Long userSeq, @RequestParam Integer currentPage) {
+    return storeNotificationPageService.storeNotificationExperienceProposal(userSeq, currentPage);
+  }
+
+}
