c8f29851ff5de09579dbc48cd9817943cd06439b
yeong1203 <cozyluv117@gmail.com>
2024-08-07 20:59:59 +0900

    feat: 체험 페이지 api 컨트롤러 수정

--- src/main/java/com/nuguna/freview/store/controller/StoreExperienceApiController.java
+++ src/main/java/com/nuguna/freview/store/controller/StoreExperienceApiController.java
@@ -8,8 +8,10 @@ import com.nuguna.freview.store.dto.response.page.StoreFinalProposalListResponse
 import com.nuguna.freview.store.dto.response.page.StoreProposalListResponseDTO;
 import com.nuguna.freview.store.dto.response.page.StoreReviewListResponseDTO;
 import com.nuguna.freview.store.service.StoreExperiencePageService;
+import java.time.LocalDate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,9 +39,10 @@ public class StoreExperienceApiController {
 
   // 지원자 상태 변경 = 지원 수락
   @RequestMapping(value = "/apply-confirm", method = RequestMethod.POST)
-  public ResponseEntity<?> applyConfirm(@RequestParam Long experienceSeq) {
+  public ResponseEntity<?> applyConfirm(@RequestParam Long experienceSeq, @RequestParam String status) {
+
     try {
-      storeExperiencePageService.applyConfirm(experienceSeq);
+      storeExperiencePageService.applyConfirm(experienceSeq, status);
       return new ResponseEntity<>(HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -63,17 +66,19 @@ public class StoreExperienceApiController {
 
   // 제안
   @RequestMapping(value = "/final-proposal-list", method = RequestMethod.GET)
-  public StoreFinalProposalListResponseDTO finalProposalList(@RequestParam Long userSeq, @RequestParam Integer currentPage){
-    return storeExperiencePageService.finalProposalList(userSeq, currentPage, STORE_LIST_PAGE_SIZE);
+  public StoreFinalProposalListResponseDTO finalProposalList(@RequestParam Long userSeq, @RequestParam Integer currentPage) {
+    return storeExperiencePageService.finalProposalList(userSeq, currentPage,
+        STORE_LIST_PAGE_SIZE);
   }
 
 
   // 체험일자 확정
   // 체험일자는 DATE 타입으로 들어온다.
   @RequestMapping(value = "/experience-date-update", method = RequestMethod.POST)
-  public ResponseEntity<?> updateExperienceDate(@RequestParam Long experienceSeq){
+  public ResponseEntity<?> updateExperienceDate(@RequestParam Long experienceSeq,  @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate experienceDate){
+    log.info("experinec" + experienceSeq + "/" + experienceDate);
     try{
-      storeExperiencePageService.updateExperienceDate(experienceSeq);
+      storeExperiencePageService.updateExperienceDate(experienceSeq, experienceDate);
       return new ResponseEntity<>(HttpStatus.OK);
     } catch (Exception e){
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -83,10 +88,11 @@ public class StoreExperienceApiController {
 
   // 체험 여부 확인 (방문 했냐? 노쇼냐?) jsp에서 들어올 값은 status도 있다~!
   // 컨트롤러 하나 ajax도 하나 버튼은 두개, 확정버튼status = ACCEPTED / 노쇼버튼status = NOSHOW
-  @RequestMapping(value = "/experiece-update-status", method = RequestMethod.POST)
+  @RequestMapping(value = "/experience-update-status", method = RequestMethod.POST)
   public ResponseEntity<?> updateExperienceStatus(@RequestParam Long experienceSeq, @RequestParam String status){
     try{
       storeExperiencePageService.updateExperienceStatus(experienceSeq, status);
+      log.info(experienceSeq + " 씨큐다!" + status + " 변경될값");
       return new ResponseEntity<>(HttpStatus.OK);
     } catch (Exception e){
       e.printStackTrace();
@@ -106,10 +112,9 @@ public class StoreExperienceApiController {
 
   // 리뷰 숨김 버튼
   @RequestMapping(value = "/review-hidden", method = RequestMethod.POST)
-  public ResponseEntity<?> updateReviewStatus(@RequestParam Long customerSeq, @RequestParam String status) {
-    log.info("여기진입");
+  public ResponseEntity<?> updateReviewStatus(@RequestParam Long seq, @RequestParam String status) {
     try{
-      storeExperiencePageService.updateReviewStatus(customerSeq, status);
+      storeExperiencePageService.updateReviewStatus(seq, status);
       return new ResponseEntity<>(HttpStatus.OK);
     } catch (Exception e){
       e.printStackTrace();
