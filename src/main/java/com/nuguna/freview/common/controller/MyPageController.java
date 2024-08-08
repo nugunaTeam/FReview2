9fc058036091b27f44a601b09a193951619bc721
seungdangdang <yseungun.21@gmail.com>
2024-08-08 15:45:21 +0900

    feat: 유저 별 마이페이지를 리다이렉트하는 컨트롤러 구현 및 헤더.jsp 반영

---
+++ src/main/java/com/nuguna/freview/common/controller/MyPageController.java
@@ -0,0 +1,27 @@
+package com.nuguna.freview.common.controller;
+
+import com.nuguna.freview.common.vo.user.UserCode;
+import com.nuguna.freview.common.vo.user.UserVO;
+import com.nuguna.freview.security.jwtfilter.JwtContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyPageController {
+
+  @RequestMapping(value = "/my", method = RequestMethod.GET)
+  public String moveToMyPage() {
+    UserVO loginUser = JwtContextHolder.getUserVO();
+    UserCode userCode = UserCode.from(loginUser.getCode());
+    if (userCode.isAdmin()) {
+      return "redirect:/admin";
+    } else if (userCode.isCustomer()) {
+      return "redirect:/my/brand-info";
+    } else if (userCode.isStore()) {
+      return "redirect:/store/my/brand";
+    } else {
+      return "redirect:/main";
+    }
+  }
+}
