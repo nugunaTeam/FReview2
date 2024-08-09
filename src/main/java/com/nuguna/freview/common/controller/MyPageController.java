3cc6833b3f3a54601e3d226e06b8c5892f913ece
seungdangdang <yseungun.21@gmail.com>
2024-08-09 10:17:20 +0900

    fix: 주석 처리된 설정 파일 해제

--- src/main/java/com/nuguna/freview/common/controller/MyPageController.java
+++ src/main/java/com/nuguna/freview/common/controller/MyPageController.java
@@ -6,12 +6,13 @@ import com.nuguna.freview.security.jwtfilter.JwtContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyPageController {
 
   @RequestMapping(value = "/my", method = RequestMethod.GET)
-  public String moveToMyPage() {
+  public String moveToMyPage(@RequestParam Long userSeq) {
     UserVO loginUser = JwtContextHolder.getUserVO();
     UserCode userCode = UserCode.from(loginUser.getCode());
     if (userCode.isAdmin()) {
@@ -19,7 +20,7 @@ public class MyPageController {
     } else if (userCode.isCustomer()) {
       return "redirect:/my/brand-info";
     } else if (userCode.isStore()) {
-      return "redirect:/store/my/brand";
+      return "redirect:/store/my/brand?" + userSeq;
     } else {
       return "redirect:/main";
     }
